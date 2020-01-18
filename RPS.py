0946a66cf4a3f4aa33fd90fe3d67b3fc436dd313
radhikaagrawal7 <58105248+radhikaagrawal7@users.noreply.github.com>
2020-01-18 04:53:46 -0500

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,26 +1,35 @@
-import random
-player1 = input("Player 1: rock, paper, or scissors ")
-a = random.randint(0,2)
-if a==0:
-   player2="rock"
-elif a==1:
-   player2="paper"
+from random import randint
+# print("Rock...")
+# print("Paper...")
+# print("Scissors...")
+
+player = input("Player, make your move: ").lower()
+rand_num = randint(0,2)
+if rand_num == 0:
+	computer = "rock"
+elif rand_num == 1:
+	computer = "paper"
 else:
-   player2="scissors"
-print("Computer chooses "+ player2)
-if player1 == "rock" and player2 == "scissors":
-	print("player1 wins!")
-elif player1 == "rock" and player2 == "paper":
-	print("Computer wins!")
-elif player1 == "paper" and player2 == "rock":
-	print("player1 wins!")
-elif player1 == "paper" and player2 == "scissors":
-	print("Computer wins!")
-elif player1 == "scissors" and player2 == "rock":
-	print("Computer wins!")
-elif player1 == "scissors" and player2 == "paper":
-	print("player1 wins!")
-elif player1 == player2:
+	computer = "scissors"
+
+print(f"Computer plays {computer}" )
+
+if player == computer:
 	print("It's a tie!")
+elif player == "rock":
+	if computer == "scissors":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "paper":
+	if computer == "rock":
+		print("player wins!")
+	else:
+		print("computer wins!")
+elif player == "scissors":
+	if computer == "paper":
+		print("player wins!")
+	else:
+		print("computer wins!")	
 else:
-	print("something went wrong")
+	print("Please enter a valid move!")
